d975a978728ac56cc1a8163d1017a077b7e8846d
Edo Williams <edowilliams@hotmail.com>
2012-02-16 17:18:27 -0700

    Added new user sign up

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -4,22 +4,41 @@ describe "LayoutLinks" do
 
   it "should have a Home Page at '/'" do
     get '/'
-    response.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => "home")
   end
   
   it "should have a Contact Page at '/contact'" do
     get '/contact'
-    response.should have_selector('title', :content => "Contact")
+    response.should have_selector('title', :content => "contact")
   end
   
   it "should have a About Page at '/about'" do
     get '/about'
-    response.should have_selector('title', :content => "About")
+    response.should have_selector('title', :content => "about")
   end
   
   it "should have a Help Page at '/help'" do
     get '/help'
-    response.should have_selector('title', :content => "Help")
+    response.should have_selector('title', :content => "help")
+  end
+  
+  it "should have a help page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
+  
+  it "should have a the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "home")
+    click_link "About"
+    response.should have_selector('title', :content => "about")
+    click_link "Contact"
+    response.should have_selector('title', :content => "contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+    response.should have_selector('a[href="/"]>img')
   end
 
 end
